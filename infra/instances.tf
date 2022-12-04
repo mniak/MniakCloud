b6915bd0d87dc4e30f56d77db4423bb6df0edb58
Andre Soares <andregsilv@gmail.com>
2022-12-04 13:47:31 -0300

    Use arch linux image

--- infra/instances.tf
+++ infra/instances.tf
@@ -1,3 +1,14 @@
+resource "oci_core_image" "ArchLinuxARMCloud" {
+  compartment_id = oci_identity_compartment.Main.id
+  image_source_details {
+    // https://github.com/mcginty/arch-boxes-arm/releases/tag/v20220323
+    source_image_type = "QCOW2"
+    source_type       = "objectStorageUri"
+    source_uri        = "https://github.com/mcginty/arch-boxes-arm/releases/download/v20220323/Arch-Linux-aarch64-cloudimg-20220323.0.qcow2"
+  }
+}
+
+
 resource "oci_core_instance" "MainServer" {
   availability_domain = "NBMi:SA-SAOPAULO-1-AD-1"
   compartment_id      = oci_identity_compartment.Main.id
@@ -12,7 +23,12 @@ resource "oci_core_instance" "MainServer" {
   }
   source_details {
     source_type = "image"
-    source_id   = "ocid1.image.oc1.sa-saopaulo-1.aaaaaaaa3qj5t6lciltgvztum7sgkp6gxb2ln4bkpraugvqvgvmt2cxh337a"
+    source_id   = oci_core_image.ArchLinuxARMCloud.id
+  }
+  metadata = {
+    "ssh_authorized_keys" = ""
+    "user_data" = "#cloud-config\n${yamlencode({
+      "groups" : "",
+    })}"
   }
 }
-
