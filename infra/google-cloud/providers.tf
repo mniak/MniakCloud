078faf1e0cfbb9fd2c3112a9d16d974e9d5409b1
Andre Soares <andregsilv@gmail.com>
2022-12-30 17:52:14 -0300

    add zone

--- infra/google-cloud/providers.tf
+++ infra/google-cloud/providers.tf
@@ -1,13 +1,20 @@
 terraform {
   required_providers {
     google = {
-      source = "hashicorp/google"
+      source  = "hashicorp/google"
       version = "4.47.0"
     }
   }
 }
 
 provider "google" {
-  project = "mniak-cloud"
-  region = vars.Region
+  project = var.ProjectName
+  region  = var.Region
+  zone    = var.Zone
+}
+
+provider "google-beta" {
+  project = var.ProjectName
+  region  = var.Region
+  zone    = var.Zone
 }
--- infra/google-cloud/vars.tf
+++ infra/google-cloud/vars.tf
@@ -1,5 +1,17 @@
+variable "ProjectName" {
+  description = "The project name"
+  type        = string
+  default     = "mniak-cloud"
+}
+
 variable "Region" {
   description = "The region to use"
+  type        = string
+  default     = "me-west1"
+}
+
+variable "Zone" {
+  description = "The zone to use"
   type        = string
   default     = "me-west1-c"
 }
